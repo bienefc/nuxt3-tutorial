1cb5c3e738c8a29fbc4364a9ea4ff332c2971a77
markiskino35 <markiskino35@gmail.com>
2024-08-18 03:34:33 +0800

    feat: add page with route parameter

---
+++ pages/products/[id].vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>Product details for {{ id }}</p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium,
+      explicabo in maxime alias doloribus veritatis magni aliquid obcaecati
+      possimus sed!
+    </p>
+  </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params;
+</script>
+
+<style scoped></style>
